3cdf718deffd9ee2e383f1728938f5a48e91ecc0
larsvinksl <l.vink@sintlucasedu.nl>
2025-05-26 16:53:28 +0200

    start procedural generation

--- procedural generation platformer/Assets/Scripts/LevelGenerator.cs
+++ procedural generation platformer/Assets/Scripts/LevelGenerator.cs
@@ -1,14 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.U2D.Path;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    public corridor1 corridor1;
-
     public GameObject H1;
-    Transform H1SP;
-    Transform H1Snap;
 
     public GameObject H2;
 
@@ -23,19 +20,30 @@ public class LevelGenerator : MonoBehaviour
 
     int rnd;
 
-    void Start()
-    {
-        H1SP = corridor1.snapPoint;
-        H1Snap = corridor1.snapper;
-    }
-
     void Update()
     {
         if (Detector.position.x > LastPoint.position.x)
         {
-            Vector3 spawnPosition = H1.transform.TransformPoint(H1SP.localPosition);
-            Instantiate(H1, spawnPosition, Quaternion.identity);
-            LastPoint = H1SP;
+            rnd = Random.Range(1, 3);
+            switch (rnd)
+            {
+                case 1:
+                    GameObject instance1 = Instantiate(H1);
+                    Transform H1SP = instance1.transform.Find("SnapPoint");
+                    Transform H1Snap = instance1.transform.Find("Snapper");
+                    Vector3 offset1 = H1Snap.position - instance1.transform.position;
+                    instance1.transform.position = LastPoint.position - offset1;
+                    LastPoint = H1SP;
+                    break;
+                case 2:
+                    GameObject instance2 = Instantiate(H2);
+                    Transform H2SP = instance2.transform.Find("SnapPoint");
+                    Transform H2Snap = instance2.transform.Find("Snapper");
+                    Vector3 offset2 = H2Snap.position - instance2.transform.position;
+                    instance2.transform.position = LastPoint.position - offset2;
+                    LastPoint = H2SP;
+                    break;
+            }
         }
     }
 }
--- procedural generation platformer/Assets/Scripts/corridor1.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class corridor1 : MonoBehaviour
-{
-    public Transform snapPoint;
-    public Transform snapper;
-}
